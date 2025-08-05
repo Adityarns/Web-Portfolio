94d74080bbd4e4c1cace8ef16c26b4a33d79ff1a
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-08-05 14:04:56 +0700

    Menambahkan tech stack section dan membuat web lebih responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,56 @@
 import DataImage from "./data";
+import { listTools } from "./data";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="justify-center text-center">
       {/* Hero */}
-      <div className="hero grid md:grid-cols-2 grid-cols-1 py-36 items-center ">
-        <div className="items-center justify-center pl-30">
-          <p className="text-left text-1xl">Hello World, I'm</p>
-          <h1 className=" text-4xl/tight font-bold mb-6 text-left">
+      <div className="hero grid lg:grid-cols-2 grid-cols-1 py-24 md:py-36 items-center xl:gap-0 gap-8 px-6 max-w-7xl mx-auto">
+        <div className="flex flex-col items-center lg:items-start justify-center text-center md:text-left">
+          <p className="text-lg md:text-xl">Hello World, I'm</p>
+          <h1 className="text-3xl md:text-4xl/tight font-bold mb-6">
             <p>Aditya Rahman Syach</p>
-            <p className="text-[#93DEFF]">Junior Web Developer</p>
+            <p className="text-[#144e94]">Junior Web Developer</p>
           </h1>
-          <div className="flex text-left gap-10 ">
-            <a href="#about" className="py-4 px-13 bg-[#146C94] rounded-3xl">
+          <div className="flex flex-wrap justify-center md:justify-start gap-4">
+            <a
+              href="#about"
+              className="py-3 px-6 md:py-4 md:px-10 bg-[#144e94] hover:bg-[#146C94] rounded-3xl text-white"
+            >
               About Me
             </a>
-            <a href="#project" className="py-4 px-13 bg-[#146C94] rounded-3xl">
+            <a
+              href="#project"
+              className="py-3 px-6 md:py-4 md:px-10 bg-[#144e94] hover:bg-[#146C94] rounded-3xl text-white"
+            >
               My Projects
             </a>
           </div>
         </div>
+
         <img
           src={DataImage.HeroImage}
           alt="Hero Image"
-          className="w-[300px] h-[300px] md:w-[350px] md:h-[350px] rounded-full object-cover mx-auto border-1"
+          className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] rounded-full object-cover mx-auto border-1"
         />
       </div>
 
       {/* About */}
-      <div className="about py-26">
-        <div className="bg-[#323643] p-10 pb-15 rounded-3xl">
-          <h1 className="text-left text-2xl/tight font-bold">About Me</h1>
-          <div className="grid md:grid-cols-2 grid-cols-1 pt-10">
+      <div className="about pt-24 px-6 max-w-7xl mx-auto">
+        <div className="bg-[#0B192C] p-6 md:p-10 rounded-3xl">
+          <h1 className="text-left text-3xl md:text-4xl/tight font-bold">
+            About Me
+          </h1>
+          <div className="grid lg:grid-cols-2 grid-cols-1 pt-10 items-center">
             <img
               src={DataImage.HeroImage}
               alt="Hero Image"
-              className="w-[300px] h-[300px] md:w-[350px] md:h-[350px] rounded-full object-cover mx-auto border-1"
+              className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] rounded-full object-cover mx-auto border-1"
             />
-            <div className="pr-5 text-left">
-              <p className="pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem corporis in architecto iste eligendi pariatur nihil
-                magnam numquam unde! Quaerat minima tenetur vitae temporibus
-                blanditiis recusandae perspiciatis, alias quasi tempore
-                obcaecati repudiandae iure distinctio debitis, doloremque
-                asperiores, quia officia quis autem omnis a nostrum aperiam.
-                Nulla sint accusantium quos soluta.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem corporis in architecto iste eligendi pariatur nihil
-                magnam numquam unde! Quaerat minima tenetur vitae temporibus
-                blanditiis recusandae perspiciatis, alias quasi tempore
-                obcaecati repudiandae iure distinctio debitis, doloremque
-                asperiores, quia officia quis autem omnis a nostrum aperiam.
-                Nulla sint accusantium quos soluta.
+            <div className="md:pr-5 pt-6 md:pt-0 text-left">
+              <p className="text-lg md:text-2xl">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit praesentium nihil delectus eius debitis eligendi dolor maiores voluptatum, nisi earum? Earum vero totam iste minima deleniti rem. Asperiores eius incidunt beatae necessitatibus eveniet repudiandae sequi quos quisquam doloremque perspiciatis dolor dolores consequuntur nulla tenetur recusandae corporis eligendi maiores, molestiae voluptatem.
               </p>
             </div>
           </div>
@@ -63,23 +58,27 @@ export default function App() {
       </div>
 
       {/* Skills */}
-      <div className="py-14">
-        <h1 className="text-left text-2xl/tight font-bold p-10"> Skills</h1>
-        <ul>
-          <li></li>
-        </ul>
+      <div className="tools mt-32">
+        <h1 className="text-4xl/snug font-bold mb-4 text-left">Tech Stack</h1>
+        <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listTools.map((tool) => (
+            <div
+              className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
+              key={tool.id}
+            >
+              <img
+                src={tool.gambar}
+                alt="Tools Image"
+                className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+              />
+              <div>
+                <h4>{tool.nama}</h4>
+                <p>{tool.ket}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
-}
-
-function Home() {
-  return <div className="text-3xl"> Ini Home</div>;
-}
-
-function About() {
-  return <div> INi About</div>;
-}
-function Contact() {
-  return <div> INi Contact</div>;
 }
