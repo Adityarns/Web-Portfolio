3e8dd0888a3f2c4ab26859a9081ba3954f10a3f7
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-08-07 10:43:26 +0700

    menambahkan kontak section

--- src/App.jsx
+++ src/App.jsx
@@ -122,6 +122,68 @@ export default function App() {
           ))}
         </div>
       </div>
+
+      {/* Kontak */}
+      <div className="kontak mt-32 p-10">
+        <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
+        <p className="text-base/loose text-center mb-10 opacity-50">
+          Mari terhubung dengan saya.
+        </p>
+
+        <form
+          action="https://formsubmit.co/rahmansyachaditya@gmail.com"
+          method="POST"
+          className="bg-zinc-800 p-10 w-fit mx-auto rounded-md"
+          autoComplete="off"
+        >
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Nama Lengkap</label>
+              <input
+                type="text"
+                name="nama"
+                placeholder="Masukkan Nama..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Masukkan Email..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
+            </div>
+
+            <div class="flex flex-col gap-2">
+              <label htmlFor="pesan" class="font-semibold">
+                Pesan
+              </label>
+              <textarea
+                name="pesan"
+                id="pesan"
+                cols="45"
+                rows="7"
+                placeholder="Pesan..."
+                className="border border-zinc-500 p-2 rounded-md required"
+              ></textarea>
+            </div>
+
+            <div class="text-center">
+              <button
+                type="submit"
+                className=" p-3 rounded-lg block border border-zinc-600 bg-[#144e94] hover:bg-[#146C94]"
+              >
+                Kirim Pesan
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
